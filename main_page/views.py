9749d2a072163b0445efc003727dc8b4aa1ee715
Grzegorz Sakowski <you@example.com>
2020-04-03 14:27:26 +0200

    Historia rezerwacji, filtr wg booking_id oraz exact_time co do sekundy

--- main_page/views.py
+++ main_page/views.py
@@ -37,22 +37,12 @@ class BookingListView(ListView):
     def get_context_data(self, **kwargs):
         # rdb.set_trace()
         context = super(BookingListView, self).get_context_data()
-        # context['my_booking_app1'] = app1.models.Booking.objects.filter(user=self.request.user)
-        # context['my_kayak_app1'] = app1.models.TermKayaks.objects.filter(booking__user=self.request.user)
-        # context['my_booking_app2'] = app2.models.Booking.objects.filter(user=self.request.user)
-        # context['my_kayak_app2'] = app2.models.TermKayaks.objects.filter(booking__user=self.request.user)
-        # context['my_booking_client_panel'] = client_panel.models.Booking.objects.filter(user=self.request.user)
-        # context['my_kayak_client_panel'] = client_panel.models.TermKayaks.objects.filter(booking__user=self.request.user)
         my_booking_app1 = app1.models.Booking.objects.filter(user=self.request.user)
         my_kayak_app1 = app1.models.TermKayaks.objects.filter(booking__user=self.request.user)
         my_booking_app2 = app2.models.Booking.objects.filter(user=self.request.user)
         my_kayak_app2 = app2.models.TermKayaks.objects.filter(booking__user=self.request.user)
         my_booking_client_panel = client_panel.models.Booking.objects.filter(user=self.request.user)
         my_kayak_client_panel = client_panel.models.TermKayaks.objects.filter(booking__user=self.request.user)
-        my_booking_list = [my_booking_app1, my_booking_app2, my_booking_client_panel]
-        my_kayak_list = [my_kayak_app1, my_kayak_app2, my_kayak_client_panel]
-        # context['my_booking_list'] = my_booking_list
-        # context['my_kayak_list'] = my_kayak_list
         union = my_booking_app1.union(my_booking_app2, my_booking_client_panel).order_by('-exact_time')
         context['union'] = union
         context['kayak'] = my_kayak_app2.union(my_kayak_app1, my_kayak_client_panel).distinct('booking_id')
