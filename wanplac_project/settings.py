55e122851b74e600164fd3c32b42b39011dd41f5
Grzegorz Sakowski <you@example.com>
2020-04-07 11:47:40 +0200

    Zmiana na STATICFILES_DIRS

--- wanplac_project/settings.py
+++ wanplac_project/settings.py
@@ -158,8 +158,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'vendor')]
 
