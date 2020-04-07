7d138aa180e6b37956c465c9bdbbe610e86e9097
Grzegorz Sakowski <you@example.com>
2020-04-07 11:20:56 +0200

    Zmiana w ALLOWED_HOSTS

--- wanplac_project/settings.py
+++ wanplac_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['https://wanplac.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['wanplac.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
