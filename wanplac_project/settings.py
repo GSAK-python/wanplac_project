f73c3c86112593b69d8c7363803952e74d3fa226
Grzegorz Sakowski <you@example.com>
2020-04-20 13:24:52 +0200

    Zmiana ustawien media

--- wanplac_project/settings.py
+++ wanplac_project/settings.py
@@ -166,7 +166,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'live_static_files', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/client_panel/registration/login/'
 
