caca1703d69046552c50b24fe77480d204535231
Grzegorz Sakowski <you@example.com>
2020-04-21 13:30:12 +0200

    Dodanie folderu media w S3

--- wanplac_project/settings.py
+++ wanplac_project/settings.py
@@ -204,6 +204,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'client_panel.storage_backends.StaticStorage'
 
 AWS_DEFAULT_ACL = None
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'client_panel.storage_backends.MediaStorage'
