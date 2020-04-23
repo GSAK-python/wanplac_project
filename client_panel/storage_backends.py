375b68c34d38af98d1f5f0119964300b9497f578
Grzegorz Sakowski <you@example.com>
2020-04-23 13:05:51 +0200

    Dodanie footera

--- client_panel/storage_backends.py
+++ client_panel/storage_backends.py
@@ -2,7 +2,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 from wanplac_project import settings
 
-
+"""
+StaticStorage - uncomment if u want to retrive static files for app form AWS S3, 
+then go to setting and uncomment rest of setting for S3 static
+"""
 # class StaticStorage(S3Boto3Storage):
 #     location = settings.STATICFILES_LOCATION
 
