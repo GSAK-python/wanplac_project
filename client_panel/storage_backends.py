381d210d0980b222df3de499f839c272710f83bc
Grzegorz Sakowski <you@example.com>
2020-04-23 10:49:34 +0200

    Zmiana w storage_backends.py

--- client_panel/storage_backends.py
+++ client_panel/storage_backends.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from wanplac_project import settings
 
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+# class StaticStorage(S3Boto3Storage):
+#     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
--- wanplac_project/settings.py
+++ wanplac_project/settings.py
@@ -208,7 +208,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_LOCATION = 'static'
+# STATICFILES_LOCATION = 'static'
 # STATICFILES_STORAGE = 'client_panel.storage_backends.StaticStorage'
 
 AWS_DEFAULT_ACL = None
