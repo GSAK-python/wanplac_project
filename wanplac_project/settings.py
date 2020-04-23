58caaca20fbc7d12fc8bbe93ff2b32d70cef6300
Grzegorz Sakowski <you@example.com>
2020-04-23 10:47:06 +0200

    Setttings - pliki static lokalnie, media z S3

--- wanplac_project/settings.py
+++ wanplac_project/settings.py
@@ -162,8 +162,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
