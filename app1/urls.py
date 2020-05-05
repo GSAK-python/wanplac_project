7cb5569b54ea37b11380546d771563d0199bf330
Grzegorz Sakowski <you@example.com>
2020-05-05 12:52:05 +0200

    Proby z edycja zamowienia

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from django.views.generic import RedirectView
-from app1.views import BookingCreateView
+from app1.views import BookingCreateView, BookingUpdateView
 
 app_name = 'app1'
 
 urlpatterns = [
     path('create/', BookingCreateView.as_view(), name='app1_create'),
+    path('update/', BookingUpdateView.as_view(), name='app1_update'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -5,7 +5,7 @@ from django.db import transaction
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.utils.html import strip_tags
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from app1.forms import BookingCreateForm, TermKayaksFormSet
 from app1.models import Booking, TermKayaks
 from django.core.mail import EmailMessage, EmailMultiAlternatives
@@ -65,3 +65,51 @@ class BookingCreateView(LoginRequiredMixin, CreateView):
         return super(BookingCreateView, self).form_invalid(form)
 
 
+class BookingUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'app1/booking/update_app1.html'
+    form_class = BookingCreateForm
+    success_url = reverse_lazy('main:booking_detail')
+
+    def get_object(self, queryset=None):
+        a = Booking.objects.filter(user=self.request.user).latest('exact_time')
+        return a
+
+    def get_context_data(self, **kwargs):
+        data = super(BookingUpdateView, self).get_context_data(**kwargs)
+        data['current_time'] = datetime.datetime.now().time()
+        data['start_break'] = datetime.time(12)
+        data['stop_break'] = datetime.time(12, 30)
+
+        if self.request.POST:
+            data['kayak_set'] = TermKayaksFormSet(self.request.POST, instance=self.object, prefix='kayak_set')
+        else:
+            data['kayak_set'] = TermKayaksFormSet(instance=self.object, prefix='kayak_set')
+
+        return data
+
+    def form_valid(self, form):
+        # rdb.set_trace()
+        context = self.get_context_data()
+        kayak_set = context['kayak_set']
+
+        with transaction.atomic():
+            if not form.cleaned_data['first_name']:
+                form.instance.first_name = self.request.user.first_name
+            if not form.cleaned_data['last_name']:
+                form.instance.last_name = self.request.user.last_name
+            form.instance.user = self.request.user
+            form.instance.email = self.request.user.email
+
+            if kayak_set.is_valid():
+                booking_form = form.save()
+                kayak_set.instance = booking_form
+                kayak_set.save()
+                for detail in kayak_set.instance.app1_term_bookings.all():
+                    detail.kayak.store -= detail.quantity
+                    if not detail.kayak.store:
+                        detail.kayak.available = False
+                    detail.kayak.save()
+
+                return super(BookingUpdateView, self).form_valid(form)
+
+        return super(BookingUpdateView, self).form_invalid(form)
