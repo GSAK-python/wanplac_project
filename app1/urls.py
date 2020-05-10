553d42108267abf073e30d807f9bc61ec716876c
Grzegorz Sakowski <you@example.com>
2020-05-10 14:08:51 +0200

    Proba tworzenia jednej templatki z potwierdzeniem dla trzech apek

--- app1/urls.py
+++ app1/urls.py
@@ -1,11 +1,10 @@
 from django.urls import include, path
 from django.views.generic import RedirectView
-from app1.views import BookingCreateView, BookingUpdateView, BookingConfirmationView
+from app1.views import BookingCreateView, BookingUpdateView
 
 app_name = 'app1'
 
 urlpatterns = [
     path('create/', BookingCreateView.as_view(), name='app1_create'),
     path('update/', BookingUpdateView.as_view(), name='app1_update'),
-    path('confirm/', BookingConfirmationView.as_view(), name='confirm'),
 ]
