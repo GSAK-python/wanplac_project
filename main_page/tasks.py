4c421b2a20d586288ac8730091e71b35ec3e052d
Grzegorz Sakowski <you@example.com>
2020-05-17 11:09:46 +0200

    Dodanie email reminder do kazdego zamowienia na dzis

--- main_page/tasks.py
+++ main_page/tasks.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import, unicode_literals
+import datetime
 from celery import shared_task
 from celery.contrib import rdb
-
+from django.core.mail import EmailMultiAlternatives
 from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.html import strip_tags
 
 import app1
 import app2
@@ -75,3 +78,44 @@ def booking_delete(request, pk):
                     kayak.kayak.save()
             booking.delete()
     return HttpResponseRedirect(reverse_lazy('main:delete_booking'))
+
+
+@shared_task
+def send_reminder_mail():
+    my_booking_app1 = app1.models.Booking.objects.all()
+    my_booking_app2 = Booking.objects.all()
+    my_booking_client_panel = client_panel.models.Booking.objects.all()
+    current_day = datetime.datetime.now().date()
+    app1_booking_reminder_list = []
+    app2_booking_reminder_list = []
+    client_panel_booking_reminder_list = []
+    for booking in my_booking_app1:
+        app1_booking_reminder_list.append(booking.code)
+        if booking.booking_date == current_day:
+            subject, from_email, to = 'Przypomnienie o potwierdzeniu rezerwacji - Wan-Plac Krutyń', 'wanplac.rezerwacjen@gmail.com', booking.user.email
+            html_content = render_to_string('reminder_email.html', {'detail': booking})
+            text_content = strip_tags(html_content)
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+        return 'Reminder email send fo bookings : {}'.format(app1_booking_reminder_list)
+    for booking in my_booking_app2:
+        app2_booking_reminder_list.append(booking.code)
+        if booking.booking_date == current_day:
+            subject, from_email, to = 'Przypomnienie o potwierdzeniu rezerwacji - Wan-Plac Krutyń', 'wanplac.rezerwacjen@gmail.com', booking.user.email
+            html_content = render_to_string('reminder_email.html', {'detail': booking})
+            text_content = strip_tags(html_content)
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+        return 'Reminder email send fo bookings : {}'.format(app2_booking_reminder_list)
+    for booking in my_booking_client_panel:
+        client_panel_booking_reminder_list.append(booking.code)
+        if booking.booking_date == current_day:
+            subject, from_email, to = 'Przypomnienie o potwierdzeniu rezerwacji - Wan-Plac Krutyń', 'wanplac.rezerwacjen@gmail.com', booking.user.email
+            html_content = render_to_string('reminder_email.html', {'detail': booking})
+            text_content = strip_tags(html_content)
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+        return 'Reminder email send fo bookings : {}'.format(client_panel_booking_reminder_list)
